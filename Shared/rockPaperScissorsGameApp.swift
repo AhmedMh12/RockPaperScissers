f3cfb26176631145a23024613705fe7bc02d1a52
ahmedmahouchi <ahmed.mahouchi@machinestalk.com>
2022-06-14 10:38:48 +0100

    Initial Commit

---
+++ Shared/rockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorsGameApp.swift
+//  Shared
+//
+//  Created by Ahmed Mahouchi on 14/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
